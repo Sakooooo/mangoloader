2ddab4ac4fdc8ba603e058e876c6595c138ee02b
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-09-04 18:36:18 +0400

    It workeded

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@ use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(fs::Files::new("/web", "./web").show_files_listing()))
+    std::env::set_var("RUST_LOG", "actix_web=info");
+
+    HttpServer::new(|| 
+		    App::new().service(fs::Files::new("/", "./web").index_file("index.html"))
+    )
         .bind(("127.0.0.1", 6725))?
         .run()
         .await
