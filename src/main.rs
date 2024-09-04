12f7056f71d77782115214e48137d521e42406c3
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-09-04 13:54:59 +0400

    dammit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use actix_web::{App, HttpServer};
 use actix_files as fs;
+use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
