045a062579936b7c76198991602b9d7370a0be55
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-09-04 13:22:41 +0400

    it works for now

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,11 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
-async fn index() -> impl Responder {
-    "Hello world!"
-}
+// use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{App, HttpServer};
+use actix_files as fs;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello));
-	App::new().service(
-	    web::scope("/")
-		.route("/index.html", web::get().to(index)),
-	)
-    })
-    .bind(("127.0.0.1", 6725))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(fs::Files::new("/web", "./web").show_files_listing()))
+        .bind(("127.0.0.1", 6725))?
+        .run()
+        .await
 }
