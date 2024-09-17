2b41845540b60cd5c9b975075693d0c21b75e0fc
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-09-17 13:22:21 +0400

    cargo-leptos to flake.nix

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
 
       devShells = eachSystem (pkgs: {
         default = pkgs.mkShell {
-          packages = attrValues { inherit (pkgs) cargo rustc rust-analyzer rustfmt pnpm; };
+          packages = attrValues { inherit (pkgs) cargo cargo-leptos rustc rust-analyzer rustfmt; };
         };
       });
 
