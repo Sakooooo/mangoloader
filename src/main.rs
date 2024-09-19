3895f34663196c6e001983935d0ea9794cadf82e
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-09-19 20:24:40 +0400

    fix weird leptos cargo watch panic thingy????

--- src/main.rs
+++ src/main.rs
@@ -23,8 +23,6 @@ async fn main() -> std::io::Result<()> {
             .service(Files::new("/assets", site_root))
             // serve the favicon from /favicon.ico
             .service(favicon)
-	    // reroute / to /web
-            .service(web::redirect("/", "/web"))
             .leptos_routes(leptos_options.to_owned(), routes.to_owned(), App)
             .app_data(web::Data::new(leptos_options.to_owned()))
         //.wrap(middleware::Compress::default())
