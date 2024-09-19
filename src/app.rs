b585fb9feb150fa1ebeca12eea9e3878e5ff4530
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-09-19 15:35:53 +0400

    something is broken pls fix future me

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,6 @@
 use leptos::*;
+use actix_web;
+use leptos_actix;
 use leptos_meta::*;
 use leptos_router::*;
 
@@ -29,22 +31,23 @@ pub fn App() -> impl IntoView {
 }
 
 /// Renders the home page of your application.
-#[component]
-fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
+// #[component]
+// fn HomePage() -> impl IntoView {
+//     // Creates a reactive value to update the button
+//     let (count, set_count) = create_signal(0);
+//     let on_click = move |_| set_count.update(|count| *count += 1);
 
-    view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
-    }
-}
+//     view! {
+//         <h1>"Welcome to Leptos!"</h1>
+//         <button on:click=on_click>"Click Me: " {count}</button>
+//     }
+// }
 
+// Dashboard
 #[component]
 fn Dashboard() -> impl IntoView {
     view! {
-    <h1>"View"</h1>
+    <h1>"Title"</h1>
     }
 }
 
@@ -57,13 +60,10 @@ fn NotFound() -> impl IntoView {
     // if you navigate to the 404 page subsequently, the status
     // code will not be set because there is not a new HTTP request
     // to the server
-    #[cfg(feature = "ssr")]
-    {
-        // this can be done inline because it's synchronous
-        // if it were async, we'd use a server function
-        let resp = expect_context::<leptos_actix::ResponseOptions>();
-        resp.set_status(actix_web::http::StatusCode::NOT_FOUND);
-    }
+    // this can be done inline because it's synchronous
+    // if it were async, we'd use a server function
+    let resp = expect_context::<leptos_actix::ResponseOptions>();
+    resp.set_status(actix_web::http::StatusCode::NOT_FOUND);
 
     view! {
         <h1>"Not Found"</h1>
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#[cfg(feature = "ssr")]
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     use actix_files::Files;
@@ -35,7 +34,6 @@ async fn main() -> std::io::Result<()> {
     .await
 }
 
-#[cfg(feature = "ssr")]
 #[actix_web::get("favicon.ico")]
 async fn favicon(
     leptos_options: actix_web::web::Data<leptos::LeptosOptions>,
@@ -47,22 +45,22 @@ async fn favicon(
     ))?)
 }
 
-#[cfg(not(any(feature = "ssr", feature = "csr")))]
-pub fn main() {
-    // no client-side main function
-    // unless we want this to work with e.g., Trunk for pure client-side testing
-    // see lib.rs for hydration function instead
-    // see optional feature `csr` instead
-}
+// #[cfg(not(any(feature = "ssr", feature = "csr")))]
+// pub fn main() {
+//     // no client-side main function
+//     // unless we want this to work with e.g., Trunk for pure client-side testing
+//     // see lib.rs for hydration function instead
+//     // see optional feature `csr` instead
+// }
 
-#[cfg(all(not(feature = "ssr"), feature = "csr"))]
-pub fn main() {
-    // a client-side main function is required for using `trunk serve`
-    // prefer using `cargo leptos serve` instead
-    // to run: `trunk serve --open --features csr`
-    use mangoloader::app::*;
+// #[cfg(all(not(feature = "ssr"), feature = "csr"))]
+// pub fn main() {
+//     // a client-side main function is required for using `trunk serve`
+//     // prefer using `cargo leptos serve` instead
+//     // to run: `trunk serve --open --features csr`
+//     use mangoloader::app::*;
 
-    console_error_panic_hook::set_once();
+//     console_error_panic_hook::set_once();
 
-    leptos::mount_to_body(App);
-}
+//     leptos::mount_to_body(App);
+// }
