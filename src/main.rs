5a8057dbef1e94b45ec29778875f346917593338
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-09-20 12:58:09 +0400

    test out html reponse

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use axum::{
     routing::get,
     Router,
+    response::Html
 };
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(test));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Ready!");
+
     axum::serve(listener, app).await.unwrap();
+}
+
+async  fn  test() -> Html<&'static str> {
+    Html("<h1>Hello</h1>")
 }
