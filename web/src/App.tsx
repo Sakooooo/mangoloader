a722f553e820597874300fef7fec8bd5d4c2d85c
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-09-25 12:41:59 +0400

    web: test

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,24 @@
 import Logo from './assets/react.svg'
+import { useEffect, useState } from 'react' 
 import './App.css'
 
 function App() {
+
+  const [posts, setPosts] = useState([]);
+
+
+   useEffect(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts?_limit=1')
+         .then((response) => response.json())
+         .then((data) => {
+	    setPosts(data);
+            console.log(data);
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
+   }, []);
+
   return (
     <>
       <div className='ui'>
@@ -10,6 +27,17 @@ function App() {
 	  <h1>Mangoloader</h1>
 	</div>
 	<div className='middle'>
+              {posts.map((post) => {
+         return (
+            <div className="post-card" key={post.id}>
+               <h2 className="post-title">{post.title}</h2>
+               <p className="post-body">{post.body}</p>
+               <div className="button">
+               <div className="delete-btn">Delete</div>
+               </div>
+            </div>
+          );
+	 })}
 	</div>
       </div>
     </>
