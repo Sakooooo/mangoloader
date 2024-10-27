41962b4b8edfeb976cbb8aad191d44b9c4c62a87
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-10-27 14:54:11 +0400

    (web) vite: fix cors issue with pnpm dev

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/web',
-})
+	plugins: [react()],
+	base: "/web",
+	server: {
+		cors: false,
+		proxy: {
+			"/api": {
+				target: "http://localhost:3000",
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
+});
