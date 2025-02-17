ec82f93f7e80bd27e6910e6049010ddfb43f4394
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-02-17 12:45:40 +0400

    start over

--- flake.nix
+++ flake.nix
@@ -1,35 +1,43 @@
 {
-  description =
-    "Mangoloader nix flake because get it nix flake nix flake nix reproduce";
+  description = "Mangoloader nix flake because get it nix flake nix flake nix reproduce";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+  inputs = {nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";};
 
-  outputs = { self, nixpkgs }:
-    let
-      inherit (nixpkgs) lib;
-      inherit (builtins) attrValues;
-      eachSystem = f:
-        lib.genAttrs [ "x86_64-linux" "aarch64-linux" ]
-        (system: f nixpkgs.legacyPackages.${system});
-    in {
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    inherit (nixpkgs) lib;
+    inherit (builtins) attrValues;
+    eachSystem = f:
+      lib.genAttrs ["x86_64-linux" "aarch64-linux"]
+      (system: f nixpkgs.legacyPackages.${system});
+  in {
+    # packages = eachSystem (pkgs: rec {
+    #   default = mangoloader;
+    #   mangoloader = pkgs.callPackage ./package.nix { };
+    # });
 
-      # packages = eachSystem (pkgs: rec {
-      #   default = mangoloader;
-      #   mangoloader = pkgs.callPackage ./package.nix { };
-      # });
-
-      devShells = eachSystem (pkgs: {
-        default = pkgs.mkShell {
-          packages = attrValues {
-            inherit (pkgs)
-              cargo cargo-watch rustc rust-analyzer rustfmt pnpm pkg-config
-              sqlite-interactive openssl vscode-langservers-extracted;
-            inherit (pkgs.nodePackages) prettier;
-          };
+    devShells = eachSystem (pkgs: {
+      default = pkgs.mkShell {
+        packages = attrValues {
+          inherit
+            (pkgs)
+            cargo
+            rustc
+            rust-analyzer
+            rustfmt
+            pkg-config
+            sqlite-interactive
+            openssl
+            vscode-langservers-extracted
+            sqlx-cli
+            ;
+          inherit (pkgs.nodePackages) prettier;
         };
-      });
+      };
+    });
 
-      formatter = eachSystem (pkgs: pkgs.alejandra);
-
-    };
+    formatter = eachSystem (pkgs: pkgs.alejandra);
+  };
 }
