1d12ca179681893a142555eb5bd210b15f9376f9
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-06-22 22:32:28 +0400

    sync changes

--- src/api/web.rs
+++ src/api/web.rs
@@ -10,7 +10,14 @@ use tower_http::trace::TraceLayer;
 #[derive(Template)]
 #[template(path = "index.html")]
 struct Index {
+    version: String,
     // todo maybe we're supposed to use Option<>?
+    sources: Vec<String>,
+}
+
+#[derive(Template)]
+#[template(path = "add.html")]
+struct Add {
     sources: Vec<String>,
 }
 
@@ -21,8 +28,19 @@ async fn index() -> Html<String> {
         "source3".to_string(),
     ];
     let template = Index {
+        version: env!("CARGO_PKG_VERSION").to_string(),
         sources: dummy_sources,
     };
+    return Html(template.render().unwrap());
+}
+
+async fn add() -> Html<String> {
+    let sources: Vec<String> = vec![
+        "source1".to_string(),
+        "source2".to_string(),
+        "source3".to_string(),
+    ];
+    let template = Add { sources };
     return Html(template.render().unwrap());
 }
 
@@ -31,5 +49,6 @@ pub async fn serve() -> Router {
     return Router::new()
         .layer(TraceLayer::new_for_http())
         .nest_service("/static", ServeDir::new("static"))
-        .route("/", get(index));
+        .route("/", get(index))
+        .route("/add", get(add));
 }
