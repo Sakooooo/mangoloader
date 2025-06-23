32077d791f0b499be62e099f11714acdf7a1739b
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-06-23 21:38:24 +0400

    this doesn't work lmao

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
+use std::error::Error;
+
+// this'll just query comick for now
+// we'll probably want to seperate this later
+struct Search {}
+
+async fn search(client: reqwest::Client, query: String) {
+    let response = client
+        .get(format!(
+            "https://api.comick.fun/v1.0/search/?tachiyomi=true&q={}",
+            query
+        ))
+        .send()
+        .await
+        .unwrap();
+
+    println!("{:? }", response);
+}
+
 #[tokio::main]
 async fn main() {
+    let client = reqwest::Client::new();
+
+    let result = search(client, "test".to_string()).await;
     println!("Hello");
 }
