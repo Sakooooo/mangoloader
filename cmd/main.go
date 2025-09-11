3c79a8343028c4f22bbeed104569b68ae82bb44f
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-09-11 15:10:58 +0300

    sync changes

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
 	fmt.Println("Hello")
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello"))
+	})
+	http.ListenAndServe(":3000", r)
 }
