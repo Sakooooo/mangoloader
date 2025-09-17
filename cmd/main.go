5507cdfbfa68b01b21d19a496f01c64a4ad4420b
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-09-17 11:01:14 +0300

    make templating work

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -14,8 +15,25 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
+	tmpl, err := template.ParseGlob("templates/*")
+	if err != nil {
+		fmt.Println("failed to load templates: ", err)
+		return
+	}
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello"))
+		// w.Write([]byte("hello"))
+
+		err = tmpl.ExecuteTemplate(w, "index.html", nil)
+		if err != nil {
+			http.Error(w, "Failed to render template.", http.StatusInternalServerError)
+			fmt.Println("Failed to render template: ", err)
+			return
+		}
+
 	})
+
+	fmt.Println("Listening on :3000")
+
 	http.ListenAndServe(":3000", r)
 }
